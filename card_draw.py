4780b48d6b0cc669513c6be0c3794a98bd3547d0
github-cloudlabsuser-1969 <github_cloudlabsuser_1969@cloudlabsaiuser.com>
2025-05-23 15:17:42 +0000

    MOD: Refactorizacion y depuracion de codigo

--- card_draw.py
+++ card_draw.py
@@ -1,15 +1,22 @@
-# Intentionally flawed Python program
+# Programa para extraer cinco cartas de una baraja estándar
 
-# importing modules
-import itertools, random
+import itertools
+import random
 
-# make a deck of cards
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club'])
+def draw_cards(num_cards=5):
+    # Crear una baraja de cartas (1-13 de cada palo)
+    deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
+    random.shuffle(deck)
 
-# shuffle the cards
-random.shuffle(deck)
+    # Verificar que hay suficientes cartas
+    if num_cards > len(deck):
+        print(f"No hay suficientes cartas en la baraja para extraer {num_cards}.")
+        return
 
-# draw five cards
-print("You got:")
-for i in range(5)
-   print(deck[i][0], "of", deck[i][1]
+    print("You got:")
+    for i in range(num_cards):
+        value, suit = deck[i]
+        print(f"{value} of {suit}")
+
+if __name__ == "__main__":
+    draw_cards()
